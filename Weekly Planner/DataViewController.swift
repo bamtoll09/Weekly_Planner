6697778039af1f1dfb10f06eee316ccbf33e7023
unrin S <Sunrin@sunrinui-macbook-pro.local>
2017-01-02 20:21:03 +0900

    Initial Commit

---
+++ Weekly Planner/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Weekly Planner
+//
+//  Created by Sunrin on 2017. 1. 2..
+//  Copyright © 2017년 Bamtoll. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
